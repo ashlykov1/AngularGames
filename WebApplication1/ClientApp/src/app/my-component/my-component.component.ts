1288e48b92f65d84270399ce6f5529d03137451a
Andrew Shlykov <andriy.shlykov@q2labsolutions.com>
2019-06-23 13:15:33 -0400

    running version

---
+++ WebApplication1/ClientApp/src/app/my-component/my-component.component.ts
@@ -0,0 +1,24 @@
+import { Component , Inject} from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+@Component({
+  selector: 'my-component-home',
+  templateUrl: './my-component.component.html',
+})
+export class MyComponent {
+  public weather : WeatherForecast[];
+
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string)
+  {
+    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
+      this.weather= result;
+    }, error => console.error(error));
+  }
+}
+
+interface WeatherForecast {
+  dateFormatted: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
